5de6aedc20306fa1b2d7b1c672fac0649e5ac753
RanjansCode <amitadevi7654@gmail.com>
2026-08-14 21:36:48 +0530

    fix: update launch logo image path
    Correct the asset path for the In Our Budget brand logo to match the project structure.

--- src/components/LaunchModeOverlay.tsx
+++ src/components/LaunchModeOverlay.tsx
@@ -84,7 +84,7 @@ export default function LaunchModeOverlay({
         {/* Brand Logo */}
         <div className="flex justify-center" id="launch-logo-container">
           <img
-            src="/lunch_logo.png.png"
+            src="/assets/images/lunch_logo.png"
             alt="In Our Budget"
             className="h-12 sm:h-14 object-contain"
           />
